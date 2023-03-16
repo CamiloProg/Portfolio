a84708b90789aaaf416efcfd94f24498ca0c4ec6
CamiloProg <camilom200107@gmail.com>
2023-03-16 11:24:31 -0500

    glass card

--- src/components/Main/MainPage.jsx
+++ src/components/Main/MainPage.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import "./MainPage.css";
 export const MainPage = () => {
   return (
-    <div class="container" id="main">
-      <div class="content">
-        <div class="bannerText">
-          <div>
-            <h2>Camilo Martinez</h2>
-            <h4>Front-end Developer</h4>
-            <a href="#about">Who am I?</a>
+    <div className="container background" id="main">
+      <div className="content">
+        <div className="bannerText">
+          <div className="cardGlass">
+            <h1>Camilo Martinez</h1>
+            <h3>Front-end Developer</h3>
+            <a className="button-28" href="#about">
+              Who am I?
+            </a>
           </div>
         </div>
       </div>
