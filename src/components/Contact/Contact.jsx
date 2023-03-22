9a116f157f5e48241bdbe8722d09b04f90c42cf8
CamiloProg <camilom200107@gmail.com>
2023-03-22 09:41:21 -0500

    githublogo

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -25,11 +25,7 @@ export const Contact = () => {
           href="https://github.com/CamiloProg"
           target="_blank"
         >
-          <img
-            src="dist/assets/gitHubLogo-d83ddf4e.png"
-            alt="Github"
-            width={80}
-          />
+          <img src={githubLogo} alt="Github" width={80} />
           <p>GitHub</p>
         </a>
 
